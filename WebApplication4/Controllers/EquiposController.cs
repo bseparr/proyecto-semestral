ebe3afb37bbdc119fe1f772bd56202f26888d686
BASS-SHZ <b.separr@gmail.com>
2020-04-01 02:53:49 -0300

    Proyecto Semestral

---
+++ WebApplication4/Controllers/EquiposController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private FutGolEntities db = new FutGolEntities();
+
+        // GET: Equipos
+        public ActionResult Index()
+        {
+            return View(db.Equipos.ToList());
+        }
+
+        // GET: Equipos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipos equipos = db.Equipos.Find(id);
+            if (equipos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipos);
+        }
+
+        // GET: Equipos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Equipos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,pais")] Equipos equipos)
+        {
+            if (ModelState.IsValid)
+            {
+                var NR = db.Equipos.Where(x => x.nombre == equipos.nombre && x.pais == equipos.pais);
+
+                if (NR == null || NR.Count() == 0)
+                {
+                    db.Equipos.Add(equipos);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.id = new SelectList(db.Equipos, "nombre", "nombre", equipos.id);
+                    ModelState.AddModelError("nombre", "Ya existe un equipo con ese nombre en el país indicado");
+                    return View(equipos);
+                }
+
+            }
+
+            return View(equipos);
+        }
+
+        // GET: Equipos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipos equipos = db.Equipos.Find(id);
+            if (equipos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipos);
+        }
+
+        // POST: Equipos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,pais")] Equipos equipos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(equipos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(equipos);
+        }
+
+        // GET: Equipos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipos equipos = db.Equipos.Find(id);
+            if (equipos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipos);
+        }
+
+        // POST: Equipos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Equipos equipos = db.Equipos.Find(id);
+            db.Equipos.Remove(equipos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication4/Controllers/JugadoresController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class JugadoresController : Controller
+    {
+        private FutGolEntities db = new FutGolEntities();
+
+        // GET: Jugadores
+        public ActionResult Index()
+        {
+            var jugadores = db.Jugadores.Include(j => j.Equipos);
+            return View(jugadores.ToList());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string equipo)
+        {
+            if (equipo == "")
+            {
+                var futbolistas = db.Jugadores.Include(j => j.Equipos);
+                return View(futbolistas.ToList());
+            }
+            else
+            {
+                var futbolistas = db.Jugadores.Include(j => j.Equipos);
+                return View(futbolistas.Where(x => x.Equipos.nombre.Contains(equipo)).ToList());
+            }
+
+        }
+        // GET: Jugadores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugadores jugadores = db.Jugadores.Find(id);
+            if (jugadores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugadores);
+        }
+
+        // GET: Jugadores/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_equipo = new SelectList(db.Equipos, "id", "nombre");
+            return View();
+        }
+
+        // POST: Jugadores/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombres,apellidos,posicion,id_equipo")] Jugadores jugadores)
+        {
+            if (ModelState.IsValid)
+            {
+                var contador = jugadores.nombres.Replace(" ", "");
+                if (contador.Length > 4)
+                {
+                    var contador2 = jugadores.apellidos.Replace(" ", "");
+
+                    if (contador2.Length > 4)
+                    {
+                        db.Jugadores.Add(jugadores);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.id_equipo = new SelectList(db.Jugadores, "apellidos", "apellidos", jugadores.id_equipo);
+                        ModelState.AddModelError("apellidos", "Los apellidos deben poseer más de 2 caracteres cada uno");
+                        return View(jugadores);
+                    }
+                }
+
+                else
+
+                {
+
+                    ViewBag.id_equipo = new SelectList(db.Jugadores, "nombres", "nombres", jugadores.id_equipo);
+                    ModelState.AddModelError("nombres", "Los nombres deben poseer más de 2 caracteres cada uno");
+                    return View(jugadores);
+
+
+                }
+            }
+
+            ViewBag.id_equipo = new SelectList(db.Equipos, "id", "nombre", jugadores.id_equipo);
+            return View(jugadores);
+        }
+
+        // GET: Jugadores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugadores jugadores = db.Jugadores.Find(id);
+            if (jugadores == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_equipo = new SelectList(db.Equipos, "id", "nombre", jugadores.id_equipo);
+            return View(jugadores);
+        }
+
+        // POST: Jugadores/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombres,apellidos,posicion,id_equipo")] Jugadores jugadores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jugadores).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_equipo = new SelectList(db.Equipos, "id", "nombre", jugadores.id_equipo);
+            return View(jugadores);
+        }
+
+        // GET: Jugadores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugadores jugadores = db.Jugadores.Find(id);
+            if (jugadores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugadores);
+        }
+
+        // POST: Jugadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Jugadores jugadores = db.Jugadores.Find(id);
+            db.Jugadores.Remove(jugadores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
